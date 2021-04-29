d8cf00c5a28eb9445267df3746b8ad225d6723be
Glitch <78031997+B4H09@users.noreply.github.com>
2021-04-29 07:11:26 +0200

    Update endecode.py

--- endecode.py
+++ endecode.py
@@ -1,4 +1,5 @@
 import base64
+from time import sleep
 
 #colors
 red = "\033[1;91m"
@@ -44,24 +45,26 @@ def endecode():
             choose = int(input("\033[1;93m\n Choose Number: " +white))
 
             if choose == 1:
-
+                sleep(0.2)
                 encodeinput = input("\033[1;93m\nEnter Text To Encoding: " +white)
 
                 encode = base64.b64encode(encodeinput.encode('UTF-8')).decode('ascii')
-
+                sleep(1)
                 print(white)
                 print("\033[1;91m\nEncoding : " +white +encode)
 
             elif choose == 2:
+                sleep(0.2)
                 decodeinput = input("\033[1;93m\nEnter Text To Decoding: " +white)
                 decode = decode_hash = base64.b64decode(decodeinput)
                 decodeit = decode.decode('UTF-8')
-
+                sleep(1)
                 print("\033[1;91m\nDecoding : " +white +decodeit)
 
             else:
                 endecode()
     except:
+        sleep(1)
         print("\n\033[1;91m#Please Just Choose 1 Or 2")
         endecode()
 endecode()
